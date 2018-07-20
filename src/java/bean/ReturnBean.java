c49a2dbbff40e71aaad9cb30d1015d1fe22079be
s20163060 <s20163060@HCS23137.stdlan.hcs.local>
2018-07-20 13:42:23 +0900

    Merge origin/master
    Conflicts:
    	src/java/db/ReturnDb.java
    	src/java/entity/Return.java

--- src/java/bean/ReturnBean.java
+++ src/java/bean/ReturnBean.java
@@ -1,46 +1,46 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package bean;
-//
-//import com.sun.faces.application.annotation.ConfigAnnotationHandler;
-//import db.ReturnDb;
-//import entity.Return;
-//import java.io.Serializable;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.logging.Logger;
-//import javax.annotation.PostConstruct;
-//import javax.ejb.EJB;
-//import javax.enterprise.context.Conversation;
-//import javax.enterprise.context.ConversationScoped;
-//import javax.inject.Inject;
-//import javax.inject.Named;
-//
-///**
-// *
-// * @author s20163060
-// */
-//@Named
-//@ConversationScoped
-//public class ReturnBean implements Serializable{
-//    
-//    @EJB
-//    ReturnDb returnDb;
-//    @Inject
-//    transient Logger log;
-//    
-//    @Inject
-//    Conversation conv;
-//    
-//    @PostConstruct
-//    public void start(){
-//        if(!conv.isTransient()){
-//            log.info(log.getName() + "￤ 会話スコープ終了");
-//            conv.end();
-//        }
-//    }
-//
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import com.sun.faces.application.annotation.ConfigAnnotationHandler;
+import db.ReturnDb;
+import entity.Return;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author s20163060
+ */
+@Named
+@ConversationScoped
+public class ReturnBean implements Serializable{
+    
+    @EJB
+    ReturnDb returnDb;
+    @Inject
+    transient Logger log;
+    
+    @Inject
+    Conversation conv;
+    
+    @PostConstruct
+    public void start(){
+        if(!conv.isTransient()){
+            log.info(log.getName() + "￤ 会話スコープ終了");
+            conv.end();
+        }
+    }
+
+}
--- src/java/entity/Rental.java
+++ src/java/entity/Rental.java
@@ -1,84 +1,84 @@
-///*
-// * 貸出処理　エンティティ
-// */
-//package entity;
-//
-//import java.io.Serializable;
-//import java.util.Date;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.ManyToMany;
-//import javax.persistence.NamedQueries;
-//import javax.persistence.NamedQuery;
-//import javax.persistence.Table;
-//import javax.persistence.Temporal;
-//import javax.persistence.Transient;
-//import javax.persistence.UniqueConstraint;
-//
-///**
-// *
-// * @author s20163037
-// */
-//@NamedQueries({
-//    @NamedQuery(name = Rental.RentalQAll,
-//            query = "SELECT e FROM Rental e")
-//})
-//@Entity
-//@Table(name = "RENTAL_INFO", uniqueConstraints = @UniqueConstraint(columnNames = "RENTALNUM"))
-//public class Rental implements Serializable {
-//
-//    public static final String RentalQAll = "RentalQAll";
-//
-//    @Id
-//    private Integer rentalNum;
-//
-//    private int amount;
-//
-//    private Date rentalDate;
-//
-//    @Transient
-//    private boolean editable;
-//
-//    public Rental() {
-//
-//    }
-//
-//    public Rental(Integer rentalNum, int amount, Date rentalDate) {
-//        this.rentalNum = rentalNum;
-//        this.amount = amount;
-//        this.rentalDate = rentalDate;
-//    }
-//
-//    public Integer getRentalNum() {
-//        return rentalNum;
-//    }
-//
-//    public void setRentalNum(Integer rentalNum) {
-//        this.rentalNum = rentalNum;
-//    }
-//
-//    public int getAmount() {
-//        return amount;
-//    }
-//
-//    public void setAmount(int amount) {
-//        this.amount = amount;
-//    }
-//
-//    public Date getRentalDate() {
-//        return rentalDate;
-//    }
-//
-//    public void setRentalDate(Date rentalDate) {
-//        this.rentalDate = rentalDate;
-//    }
-//
-//    public boolean isEditable() {
-//        return editable;
-//    }
-//
-//    public void setEditable(boolean editable) {
-//        this.editable = editable;
-//    }
-//
-//}
+/*
+ * 貸出処理　エンティティ
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+
+/**
+ *
+ * @author s20163037
+ */
+@NamedQueries({
+    @NamedQuery(name = Rental.RentalQAll,
+            query = "SELECT e FROM Rental e")
+})
+@Entity
+@Table(name = "RENTAL_INFO", uniqueConstraints = @UniqueConstraint(columnNames = "RENTALNUM"))
+public class Rental implements Serializable {
+
+    public static final String RentalQAll = "RentalQAll";
+
+    @Id
+    private Integer rentalNum;
+
+    private int amount;
+
+    private Date rentalDate;
+
+    @Transient
+    private boolean editable;
+
+    public Rental() {
+
+    }
+
+    public Rental(Integer rentalNum, int amount, Date rentalDate) {
+        this.rentalNum = rentalNum;
+        this.amount = amount;
+        this.rentalDate = rentalDate;
+    }
+
+    public Integer getRentalNum() {
+        return rentalNum;
+    }
+
+    public void setRentalNum(Integer rentalNum) {
+        this.rentalNum = rentalNum;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+}
