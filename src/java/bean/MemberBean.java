8e9ab0ec27ee3032f5b66cbfedee5c9f62ac1a85
s20163037 <s20163037@HCS23144.stdlan.hcs.local>
2018-07-24 16:47:39 +0900

    7/24 貸し出し処理、画面作成中

---
+++ src/java/bean/MemberBean.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import db.MemberDb;
+import java.io.Serializable;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author s20163037
+ */
+@Named
+@SessionScoped
+public class MemberBean implements Serializable{
+    
+    @EJB
+    MemberDb memberDb;
+    
+    @Inject
+    transient Logger log;
+    
+    @Inject
+    Conversation conv;
+    
+    @PostConstruct
+    public void start() {
+        if (!conv.isTransient()) {
+            log.info(log.getName() + "| 会話スコープ終了");
+            conv.end();
+        }
+    }
+
+}
--- src/java/db/MemberDb.java
+++ src/java/db/MemberDb.java
@@ -1,51 +1,51 @@
-///*
-// * 会員　CRUD処理クラス
-// */
-//package db;
-//
-//import entity.Member;
-//import java.util.List;
-//import javax.ejb.Stateless;
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//import javax.persistence.TypedQuery;
-//
-//
-///**
-// *
-// * @author s20163037
-// */
-//@Stateless
-//public class MemberDb {
-//
-//    @PersistenceContext
-//    private EntityManager em;  // エンティティマネージャ
-//
-//    public void create(Member member) {  // 作成・登録
-//        em.persist(member);
+/*
+ * 会員　CRUD処理クラス
+ */
+package db;
+
+import entity.Member;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+
+/**
+ *
+ * @author s20163037
+ */
+@Stateless
+public class MemberDb {
+
+    @PersistenceContext
+    private EntityManager em;  // エンティティマネージャ
+
+    public void create(Member member) {  // 作成・登録
+        em.persist(member);
+    }
+
+//    public void update(Member member) {  // 更新
+//        em.merge(member);
 //    }
 //
-////    public void update(Rental rental) {  // 更新
-////        em.merge(rental);
-////    }
-////
-////    public void delete(Rental rental) {  // 削除
-////        em.remove(em.merge(rental));
-////    }
-////
-////    public Rental find(Integer key) {  // 検索
-////        return em.find(Rental.class, key);
-////    }
-//
-////    public List<Rental> find(String name) {  // の検索
-////        TypedQuery<Rental> q = em.createNamedQuery(Rental.RentalQName, Rental.class);
-////        q.setParameter(1, name);
-////        return q.getResultList();
-////    }
-//
-//    public List<Member> getAll() {  // 会員データ全件取得
-//        return em.createNamedQuery(Member.MemberQAll, Member.class).getResultList();
+//    public void delete(Member member) {  // 削除
+//        em.remove(em.merge(member));
 //    }
-//    
-//    
-//}
+//
+//    public Member find(Integer key) {  // 検索
+//        return em.find(Member.class, key);
+//    }
+
+    public List<Member> find(String name) {  // の検索
+        TypedQuery<Member> q = em.createNamedQuery(Member.MemberQName, Member.class);
+        q.setParameter(1, name);
+        return q.getResultList();
+    }
+
+    public List<Member> getAll() {  // 会員データ全件取得
+        return em.createNamedQuery(Member.MemberQAll, Member.class).getResultList();
+    }
+    
+    
+}
