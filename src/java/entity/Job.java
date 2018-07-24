118d8dde2dcdbaaa8c91d70d80c6737d7180ca2a
s20163037 <s20163037@HCS23144.stdlan.hcs.local>
2018-07-24 12:10:56 +0900

    7/24 貸し出し処理、画面作成中

--- src/java/entity/Job.java
+++ src/java/entity/Job.java
@@ -1,63 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package entity;
-
-import java.io.Serializable;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-
-/**
- *
- * @author s20163037
- */
-@Entity
-@Table(name = "job_list")
-public class Job implements Serializable{
-    
-    private static final long serialVersionUID = 1L; // シリアライズ
-    
-    @Id
-    private String job_id;
-    
-    @Size(max = 20)
-    private String job_name;
-    
-    @OneToMany(mappedBy = "job_id", cascade = CascadeType.ALL)
-    private List<Member> member;
-    
-
-    public Job() {
-    }
-
-    
-    public Job(String job_id, String job_name) {
-        this.job_id = job_id;
-        this.job_name = job_name;
-    }
-
-    public String getJob_id() {
-        return job_id;
-    }
-
-    public void setJob_id(String job_id) {
-        this.job_id = job_id;
-    }
-
-    public String getJob_name() {
-        return job_name;
-    }
-
-    public void setJob_name(String job_name) {
-        this.job_name = job_name;
-    }
-    
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package entity;
+//
+//import java.io.Serializable;
+//import java.util.List;
+//import javax.persistence.CascadeType;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//import javax.persistence.Table;
+//import javax.validation.constraints.Size;
+//
+///**
+// *
+// * @author s20163037
+// */
+//@Entity
+//@Table(name = "job_list")
+//public class Job implements Serializable{
+//    
+//    private static final long serialVersionUID = 1L; // シリアライズ
+//    
+//    @Id
+//    private String job_id;      // 職業ID
+//    
+//    @Size(max = 20)
+//    private String job_name;    // 職業名
+//    
+//    @OneToMany(mappedBy = "job_id", cascade = CascadeType.ALL)
+//    private List<Member> member;
+//    
+//
+//    public Job() {
+//    }
+//
+//    
+//    public Job(String job_id, String job_name) {
+//        this.job_id = job_id;
+//        this.job_name = job_name;
+//    }
+//
+//    public String getJob_id() {
+//        return job_id;
+//    }
+//
+//    public void setJob_id(String job_id) {
+//        this.job_id = job_id;
+//    }
+//
+//    public String getJob_name() {
+//        return job_name;
+//    }
+//
+//    public void setJob_name(String job_name) {
+//        this.job_name = job_name;
+//    }
+//    
+//    
+//}
