6db79393fdbe83af199bc2de7e04dd41b0baf65f
s20163037 <s20163037@HCS23144.stdlan.hcs.local>
2018-07-25 15:48:19 +0900

    7/25 一時保存

--- src/java/entity/Register.java
+++ src/java/entity/Register.java
@@ -1,25 +1,31 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package entity;
-//
-//import java.io.Serializable;
-//import javax.persistence.Entity;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-///**
-// *
-// * @author s20163037
-// */
-//@Entity
-//@Table(name = "register")
-//public class Register implements Serializable {
-//    
-//    private static final long serialVersionUID = 1L; // シリアライズ
-//    
-//    @Id
-//    private String register_id;
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author s20163037
+ */
+@Entity
+@Table(name = "register")
+public class Register implements Serializable {
+
+    private static final long serialVersionUID = 1L; // シリアライズ
+
+    @Id
+    private String register_id;         // レジID
+
+    private Store store_id;             // 店舗ID
+
+    @Size(max = 7)
+    private int register_amt = 0;           // レジ内金額
+}
