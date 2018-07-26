f4746154b07d72809a752861e5633096fe81a24c
s20163037 <s20163037@HCS23144.stdlan.hcs.local>
2018-07-26 12:13:20 +0900

    7/26 1コマ目分　entity生成中

---
+++ src/java/entity/DvdInfo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author s20163037
+ */
+public class DvdInfo {
+    
+}
